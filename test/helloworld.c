d1472c5af174c29d2e65ccd399d9d1098174b90e
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-12-02 13:27:16 +0530

    Basic reference LLVM pass plugin working

---
+++ test/helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int testing_function(int a, int b, int c) {
+  printf("Hello World from: %s\n", __func__);
+  printf("  number of arguments: %d\n", 3);
+  return 0;
+}
+
+int main () {
+  printf("Hello World from: %s\n", __func__);
+  testing_function(1, 2, 3);
+  return 0;
+}
