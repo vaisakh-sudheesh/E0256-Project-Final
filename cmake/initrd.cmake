b8ec7227a676c7a67aab357a0ac57f94bf6c6785
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-12-03 03:27:47 +0530

    Updating cmake rules

--- cmake/initrd.cmake
+++ cmake/initrd.cmake
@@ -8,5 +8,5 @@ function(build_minimal_fs busybox_src_dir busybox_build_dir busybox_install_dir 
         USES_TERMINAL
     )
     add_custom_target(minimal_fs_build ALL DEPENDS ${output} busybox_build)
-    add_dependencies(minimal_fs_build busybox_build)
+    add_dependencies(minimal_fs_build busybox_build LibcCallGraphGen run_tests)
 endfunction(build_minimal_fs)
